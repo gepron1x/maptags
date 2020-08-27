2d46ca48da6cd9f034a0b000e0d9e0cdee63e731
gepron1x <georgijpronuk@gmail.com>
2020-08-27 18:08:47 +0300

    Update InventoryListener.java

--- src/me/gepron1x/maptags/InventoryListener.java
+++ src/me/gepron1x/maptags/InventoryListener.java
@@ -1,14 +1,21 @@
 package me.gepron1x.maptags;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class InventoryListener implements Listener {
 
 	GlobalMapTagsGUI gui;
-	
+	MapTagsPlugin main;
+	public InventoryListener() {
+		this.main = MapTagsPlugin.getInstance();
+	}
 	@EventHandler
 	public void onClick(InventoryClickEvent e) {
 		
@@ -22,7 +29,17 @@ public class InventoryListener implements Listener {
 		switch (e.getSlot()) {
 		case 46:
 	       if(gui.getPage() == 0) {
+	    	   final Inventory inv = e.getClickedInventory();
 	    	   e.getClickedInventory().setItem(46, gui.getNoPage());
+	    	   Bukkit.getScheduler().runTaskLater(main, new Runnable() {
+
+				@Override
+				public void run() {
+				   inv.setItem(46, new ItemStack(Material.ARROW));
+					
+				}
+	    		   
+	    	   }, 20);
 	    	   return;
 	       }
 	       
@@ -33,7 +50,18 @@ public class InventoryListener implements Listener {
 		case 52:
 			
 			if(gui.getLastPage() == gui.getPage()) {
+				final Inventory inv = e.getClickedInventory();
+				
 				e.getClickedInventory().setItem(52, gui.getNoPage());
+				Bukkit.getScheduler().runTaskLater(main, new Runnable() {
+
+					@Override
+					public void run() {
+					   inv.setItem(52, new ItemStack(Material.ARROW));
+						
+					}
+		    		   
+		    	   }, 20);
 				return;
 			}
 			gui.next();
