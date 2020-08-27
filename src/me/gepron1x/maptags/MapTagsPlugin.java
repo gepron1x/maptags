37cc6eb24d3fdc232bb2e6761f1b4b3ffcac5ec0
NukerFall <nukerfall@gmail.com>
2020-08-27 15:18:53 +0300

    done

--- src/me/gepron1x/maptags/MapTagsPlugin.java
+++ src/me/gepron1x/maptags/MapTagsPlugin.java
@@ -8,44 +8,41 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
-
 public class MapTagsPlugin extends JavaPlugin {
-    File MapTagsStorage;
-    public FileConfiguration maptags;
-    private GlobalTagList globallist;
-    
-public void onEnable() {
-	
-	MapTagsStorage = new File(getDataFolder(), "tags.yml");
-	// set the file location
-	maptags = YamlConfiguration.loadConfiguration(MapTagsStorage);
-	this.getConfig().options().copyDefaults(true);
-	this.maptags.options().copyDefaults(true);
-		this.getLogger().info("Плагин включен!");
-	
-		 
+	File MapTagsStorage;
+	public FileConfiguration maptags;
+	private GlobalTagList globallist;
+
+	public void onEnable() {
+
+		MapTagsStorage = new File(getDataFolder(), "tags.yml");
+		// set the file location
+		maptags = YamlConfiguration.loadConfiguration(MapTagsStorage);
+		this.getConfig().options().copyDefaults(true);
+		this.maptags.options().copyDefaults(true);
+		this.getLogger().info("set text!");
+
 		this.saveConfigs();
-		
-	   
-		}
-public void saveConfigs() {
-     try {
-		maptags.save(MapTagsStorage);
-		this.saveConfig();
-	} catch (IOException e) {
-	
-		e.printStackTrace();
+
 	}
-}
 
+	public void saveConfigs() {
+		try {
+			maptags.save(MapTagsStorage);
+			this.saveConfig();
+		} catch (IOException e) {
 
-		public void onDisable() {
-			this.getLogger().info("Плагин отключен!");	
-			this.saveConfigs();
-		      }
-		public GlobalTagList getGloballist() {
-			return globallist;
+			e.printStackTrace();
 		}
+	}
+
+	public void onDisable() {
+		this.getLogger().info("set text!");
+		this.saveConfigs();
+	}
+
+	public GlobalTagList getGloballist() {
+		return globallist;
+	}
 
 }
