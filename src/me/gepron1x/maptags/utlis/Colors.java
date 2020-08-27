d91be388e2034816cba7bb55394410f4c6f6e58f
NukerFall <nukerfall@gmail.com>
2020-08-27 18:51:11 +0300

    Create Colors.java

---
+++ src/me/gepron1x/maptags/utlis/Colors.java
@@ -0,0 +1,11 @@
+package me.gepron1x.maptags.utlis;
+
+import org.bukkit.ChatColor;
+
+public class Colors {
+	
+	public static String paint(String s) {
+		return ChatColor.translateAlternateColorCodes('&', s);
+	}
+
+}
