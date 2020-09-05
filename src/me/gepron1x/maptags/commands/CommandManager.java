e2d640389732876e66f30101aea55a6c8c3db41f
gepron1x <georgijpronuk@gmail.com>
2020-09-05 13:50:47 +0300

    Update CommandManager.java

--- src/me/gepron1x/maptags/commands/CommandManager.java
+++ src/me/gepron1x/maptags/commands/CommandManager.java
@@ -17,21 +17,28 @@ public class CommandManager implements CommandExecutor {
 	public MapTagsPlugin main = MapTagsPlugin.getInstance();
 
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(args.length == 0) return true;
-		switch(args[0]) {
-		case "create":
-	     createCommand(sender,args);
-	     break;
-		case "remove":
-		 removeCommand(sender,args);
-		 break;
-		case "list":
-		listCommand(sender,args);
-		break;
-		case "share":
-		case "unshare":
-		shareCommand(sender,args);
-		break;
+		
+		if (args.length == 0)
+			return true;
+		if (sender instanceof Player) {
+			switch (args[0]) {
+			case "create":
+				createCommand(sender, args);
+				break;
+			case "remove":
+				removeCommand(sender, args);
+				break;
+			case "list":
+				listCommand(sender, args);
+				break;
+			case "share":
+			case "unshare":
+				shareCommand(sender, args);
+				break;
+			}
+			return true;
+		} else {
+			sender.sendMessage(Colors.paint(main.getMessages().getString("command.players-only")));
 		}
 		return true;
 	}
@@ -44,44 +51,43 @@ public class CommandManager implements CommandExecutor {
 	private void throwInfo(CommandSender sender) {
 
 	}
-	private void createCommand(CommandSender sender,String[] args) {
-		if(sender instanceof Player) {
-			boolean isLocal = false;
+
+	private void createCommand(CommandSender sender, String[] args) {
+		boolean isLocal = false;
 		Player p = (Player) sender;
-		if(args[4].equalsIgnoreCase("local")) isLocal = true;
-		   main.addTag(args[1], args[2], args[3], p, isLocal);
+		if (args[4].equalsIgnoreCase("local"))
+			isLocal = true;
+		main.addTag(args[1], args[2], args[3], p, isLocal);
+
+	}
+
+	private void removeCommand(CommandSender sender, String[] args) {
+		boolean isOk = main.removeTag(args[1], (Player) sender);
+		if (isOk == true)
+			sender.sendMessage("Метка успешно удалена.");
+	}
+
+	private void listCommand(CommandSender sender, String[] args) {
+		GlobalMapTagsGUI gui;
+		Player p = (Player) sender;
+		if (args[1].equalsIgnoreCase("local")) {
+			gui = new GlobalMapTagsGUI(main.getMySQL().getPlayerMapTags(p.getUniqueId()), "Локальные метки");
+
 		} else {
-			sender.sendMessage(Colors.paint(main.getMessages().getString("players-only")));
+			gui = new GlobalMapTagsGUI(main.getGlobalList(), "Глобальные метки");
+		}
+		p.openInventory(gui.getInventory());
+
+	}
+
+	private void shareCommand(CommandSender sender, String[] args) {
+		UUID p = Bukkit.getPlayer(args[1]).getUniqueId();
+		String id = args[2];
+		if (args[0].equalsIgnoreCase("share")) {
+			main.getMySQL().setPlayerPermission(p, id);
+		} else if (args[0].equalsIgnoreCase("unshare")) {
+			main.getMySQL().removePermission(p, id);
 		}
 	}
-   private void removeCommand(CommandSender sender,String[] args) {
-	    boolean isOk = main.removeTag(args[1], (Player) sender);
-	    if(isOk == true) sender.sendMessage("Метка успешно удалена.");
-   }
-  private void listCommand(CommandSender sender,String[] args) {
-	  if(sender instanceof Player) {
-		  GlobalMapTagsGUI gui;
-		  Player p = (Player) sender;
-		  if(args[1].equalsIgnoreCase("local")) {
-			  gui = new GlobalMapTagsGUI(main.getMySQL().getPlayerMapTags(p.getUniqueId()),"Локальные метки");
-			 
-		  } else {
-			  gui = new GlobalMapTagsGUI(main.getGlobalList(),"Глобальные метки");
-		  }
-		  p.openInventory(gui.getInventory());
-	  }
- 
-  }
-  private void shareCommand(CommandSender sender,String[] args) {
-		 if(sender instanceof Player) {
-			 UUID p = Bukkit.getPlayer(args[1]).getUniqueId();
-			 String id = args[2];
-			 if(args[0].equalsIgnoreCase("share")) {
-				 main.getMySQL().setPlayerPermission(p, id);
-			 } else if(args[0].equalsIgnoreCase("unshare")) {
-				 main.getMySQL().removePermission(p, id);
-			 }
-		 }
-  }
 
 }
