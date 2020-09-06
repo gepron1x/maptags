ba2ee2a47cb1d46c8b7731b51e3c1a9cf3bf17b2
gepron1x <georgijpronuk@gmail.com>
2020-09-06 20:34:16 +0300

    sex

--- src/me/gepron1x/maptags/events/InventoryListener.java
+++ src/me/gepron1x/maptags/events/InventoryListener.java
@@ -12,83 +12,94 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import me.gepron1x.maptags.MapTagsPlugin;
 import me.gepron1x.maptags.utlis.GlobalMapTagsGUI;
+import me.gepron1x.maptags.utlis.MainGUI;
 import me.gepron1x.maptags.utlis.MapTag;
 import net.md_5.bungee.api.ChatColor;
 
 public class InventoryListener implements Listener {
 
-	GlobalMapTagsGUI gui;
 	MapTagsPlugin main = MapTagsPlugin.getInstance();
 
 	@EventHandler
 	public void onClick(InventoryClickEvent e) {
-
+		GlobalMapTagsGUI gui;
 		if (e.getInventory().getHolder() instanceof GlobalMapTagsGUI) {
 			gui = (GlobalMapTagsGUI) e.getInventory().getHolder();
+
+			Player p = (Player) e.getWhoClicked();
+			e.setCancelled(true);
+			switch (e.getSlot()) {
+			case 46:
+				if (gui.getPage() == 0) {
+					final Inventory inv = e.getClickedInventory();
+					e.getClickedInventory().setItem(46, gui.getNoPage());
+					Bukkit.getScheduler().runTaskLater(main, new Runnable() {
+
+						@Override
+						public void run() {
+							inv.setItem(46, new ItemStack(Material.ARROW));
+						}
+					}, 20);
+					return;
+				}
+				gui.previous();
+				p.closeInventory();
+				p.openInventory(gui.getInventory());
+				break;
+			case 52:
+
+				if (gui.getLastPage() == gui.getPage()) {
+					final Inventory inv = e.getClickedInventory();
+
+					e.getClickedInventory().setItem(52, gui.getNoPage());
+					Bukkit.getScheduler().runTaskLater(main, new Runnable() {
+
+						@Override
+						public void run() {
+							inv.setItem(52, new ItemStack(Material.ARROW));
+
+						}
+
+					}, 20);
+					return;
+				}
+				gui.next();
+				p.closeInventory();
+				p.openInventory(gui.getInventory());
+				break;
+			default:
+				if (e.getCurrentItem() == null || e.getCurrentItem().getType() == Material.AIR)
+					return;
+				final ItemStack is = e.getCurrentItem();
+				final int slot = e.getSlot();
+				final Inventory inv = e.getClickedInventory();
+				MapTag tag = gui.getClickedTag(is);
+				main.getWaypoints().addWayPoint((Player) e.getWhoClicked(), tag);
+				ItemStack selected = new ItemStack(Material.STRUCTURE_VOID);
+				ItemMeta meta = selected.getItemMeta();
+				meta.setDisplayName(ChatColor.AQUA + "Метка успешно выбрана!");
+				selected.setItemMeta(meta);
+				e.getClickedInventory().setItem(e.getSlot(), selected);
+				Bukkit.getScheduler().runTaskLater(main, new Runnable() {
+
+					@Override
+					public void run() {
+						inv.setItem(slot, is);
+
+					}
+				}, 20);
+				break;
+
+			}
+		} else if (e.getClickedInventory().getHolder() instanceof MainGUI) {
+			e.setCancelled(true);
+			MainGUI maingui = (MainGUI) e.getClickedInventory().getHolder();
+			if (e.getCurrentItem().getType() == Material.ACACIA_STAIRS || e.getCurrentItem() == null)
+				return;
+			maingui.executeAction(e.getSlot(), (Player) e.getWhoClicked());
+
 		} else {
 			return;
-		}
-		Player p = (Player) e.getWhoClicked();
-		e.setCancelled(true);
-		switch (e.getSlot()) {
-		case 46:
-			if (gui.getPage() == 0) {
-				final Inventory inv = e.getClickedInventory();
-				e.getClickedInventory().setItem(46, gui.getNoPage());
-				Bukkit.getScheduler().runTaskLater(main, new Runnable() {
-
-					@Override
-					public void run() {
-						inv.setItem(46, new ItemStack(Material.ARROW));
-					}
-				}, 20);
-				return;
-			}
-			gui.previous();
-			p.closeInventory();
-			p.openInventory(gui.getInventory());
-			break;
-		case 52:
-
-			if (gui.getLastPage() == gui.getPage()) {
-				final Inventory inv = e.getClickedInventory();
-
-				e.getClickedInventory().setItem(52, gui.getNoPage());
-				Bukkit.getScheduler().runTaskLater(main, new Runnable() {
-
-					@Override
-					public void run() {
-						inv.setItem(52, new ItemStack(Material.ARROW));
-
-					}
-
-				}, 20);
-				return;
-			}
-			gui.next();
-			p.closeInventory();
-			p.openInventory(gui.getInventory());
-			break;
-	   default:
-		   final ItemStack is = e.getCurrentItem();
-		   final int slot = e.getSlot();
-		   final Inventory inv = e.getClickedInventory();
-		   MapTag tag = gui.getClickedTag(is);
-		   main.getWaypoints().addWayPoint((Player) e.getWhoClicked(), tag);
-		   ItemStack selected = new ItemStack(Material.STRUCTURE_VOID);
-		   ItemMeta meta = selected.getItemMeta();
-		   meta.setDisplayName(ChatColor.AQUA+"Метка успешно выбрана!");
-		   selected.setItemMeta(meta);
-		   e.getClickedInventory().setItem(e.getSlot(),selected);
-		   Bukkit.getScheduler().runTaskLater(main, new Runnable() {
-
-			@Override
-			public void run() {
-				inv.setItem(slot, is);
-				
-			}}, 20);
-		   break;
-		
 		}
 	}
 
--- src/me/gepron1x/maptags/utlis/MainGUI.java
+++ src/me/gepron1x/maptags/utlis/MainGUI.java
@@ -1,9 +1,11 @@
 package me.gepron1x.maptags.utlis;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
@@ -12,29 +14,52 @@ import org.bukkit.inventory.meta.ItemMeta;
 import me.gepron1x.maptags.MapTagsPlugin;
 
 public class MainGUI implements InventoryHolder {
-Inventory gui;
-MapTagsPlugin main;
-public MainGUI() {
-	main = MapTagsPlugin.getInstance();
-	String title = main.getConfig().getString("gui.main");
-	int size = main.getConfig().getInt("gui.main.rows")*9;
-	gui = Bukkit.createInventory(this, size,title);
-	for(String path : main.getConfig().getConfigurationSection("gui.main").getKeys(false)) {
-		String name = main.getConfig().getString("gui.main."+path+".name");
-		List<String> lore = main.getConfig().getStringList("gui.main."+path+".lore");
-		ItemStack icon = new ItemStack(Material.getMaterial(main.getConfig().getString("gui.main."+path+".material")),main.getConfig().getInt("gui.main."+path+".count"));
-		ItemMeta meta = icon.getItemMeta();
-		meta.setDisplayName(name);
-		meta.setLore(lore);
-		icon.setItemMeta(meta);
-		gui.setItem(main.getConfig().getInt("gui.main."+path+".slot"), icon);
-		
+	private Inventory gui;
+	private MapTagsPlugin main;
+	private HashMap<Integer, String> actions = new HashMap<Integer, String>();
+
+	public MainGUI() {
+		main = MapTagsPlugin.getInstance();
+		String title = main.getConfig().getString("gui.main");
+		int size = main.getConfig().getInt("gui.main.rows") * 9;
+		gui = Bukkit.createInventory(this, size, title);
+		for (String path : main.getConfig().getConfigurationSection("gui.main").getKeys(false)) {
+			String name = main.getConfig().getString("gui.main." + path + ".name");
+			List<String> lore = main.getConfig().getStringList("gui.main." + path + ".lore");
+			ItemStack icon = new ItemStack(
+					Material.getMaterial(main.getConfig().getString("gui.main." + path + ".material")),
+					main.getConfig().getInt("gui.main." + path + ".count"));
+			ItemMeta meta = icon.getItemMeta();
+			meta.setDisplayName(name);
+			meta.setLore(lore);
+			icon.setItemMeta(meta);
+			int slot = main.getConfig().getInt("gui.main." + path + ".slot");
+			gui.setItem(slot, icon);
+			actions.put(slot, main.getConfig().getString("gui.main." + path + "action"));
+
+		}
 	}
-}
+
 	@Override
 	public Inventory getInventory() {
 		// TODO Автоматически созданная заглушка метода
-		return null;
+		return gui;
 	}
 
+	public void executeAction(int slot, Player p) {
+		String action = actions.get(slot);
+		switch (action) {
+		case "OPEN_GLOBAL_TAGS":
+			GlobalMapTagsGUI gui = new GlobalMapTagsGUI(main.getGlobalList(), "Глобальные метки");
+			p.closeInventory();
+			p.openInventory(gui.getInventory());
+			break;
+		case "OPEN_LOCAL_TAGS":
+			GlobalMapTagsGUI local = new GlobalMapTagsGUI(main.getMySQL().getPlayerMapTags(p.getUniqueId()),
+					"Локальные метки");
+			p.closeInventory();
+			p.openInventory(local.getInventory());
+			break;
+		}
+	}
 }
